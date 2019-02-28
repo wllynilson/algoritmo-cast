94f1744556498750dde600ed0aad27f06112420b
Wllynilson <wllynilson@gmail.com>
2019-02-28 20:10:04 -0300

    update archive
    deu certo ihuul

--- cast-128.py
+++ cast-128.py
@@ -1,12 +1,15 @@
 from Crypto.Cipher import CAST
 
-chave = b'Goku'
+key = b'chave'
 cipher = CAST.new(key, CAST.MODE_OPENPGP)
-texto = b'Uma chance num milhão é melhor que nenhuma chance!'
-msg = cipher.encrypt(texto)
+plaintext = b'wllynilson carneiro'
+msg = cipher.encrypt(plaintext)
+print(msg)
 
-...
-tam = msg[:CAST.block_size+2]
-ciphertext = msg[CAST.block_size+2:]
-cipher = CAST.new(chave, CAST.MODE_OPENPGP, tam)
-print cipher.decrypt(ciphertext)
+print('-----------------------------------')
+
+eiv = msg[:CAST.block_size + 2]
+ciphertext = msg[CAST.block_size + 2:]
+cipher = CAST.new(key, CAST.MODE_OPENPGP, eiv)
+
+print (cipher.decrypt(ciphertext))
